e1e0c5686b9f2e6cd43fe8ca7778f76435c05c67
gregory <gregorykimbira7@gmail.com>
2025-07-23 12:24:07 +0300

    created login and signup views using inbuilt django authentication

---
+++ Project/App/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", include("django.contrib.auth.urls"),),
+    path("signup/", views.sign_up, name = 'signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path("home/",views.home, name ='home' ),
+    
+]
