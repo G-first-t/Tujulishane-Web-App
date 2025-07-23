72f636f88358a93f2ff39d7ed7f9364717ea0068
gregory <gregorykimbira7@gmail.com>
2025-07-24 00:16:01 +0300

    adding requirements in  image

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -74,12 +74,11 @@ WSGI_APPLICATION = "Project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
+
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 
@@ -116,8 +115,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+import os
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
